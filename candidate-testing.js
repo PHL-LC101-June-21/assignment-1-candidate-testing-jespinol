4966a819507ac6ea1bdc2d58ddd305aa8e561ac3
jespinol <76534045+jespinol@users.noreply.github.com>
2021-06-14 18:22:27 +0000

    Completed part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -15,19 +15,24 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question('Enter candidate name: ');
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  const input = require('readline-sync')
+  candidateAnswer = input.question('Who was the first American woman in space? ')
+  correctAnswer = 'Sally Ride'
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  if (candidateAnswer == correctAnswer) {
+    console.log('Correct answer!')
+  } else {
+    console.log("Sorry, that's not correct.")
+  }
 
   let grade;
   
@@ -38,7 +43,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log(`Hello, ${candidateName}`)
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
