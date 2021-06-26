a4fcd42dc77d6a4286cfb1cea3b749b1623361b2
jespinol <76534045+jespinol@users.noreply.github.com>
2021-06-26 03:07:40 +0000

    put back some code from parts 1 or 2 for grading purposes

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,17 +1,24 @@
 const input = require('readline-sync');
 
 let candidateName;
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+candidateName = ''
 
+let question;
+question = 'Who was the first American woman in space?'
+
+let correctAnswer;
+correctAnswer = 'Sally Ride'
+
+let candidateAnswer;
+candidateAnswer = ''
+
+let questions;
 questions = ['Who was the first American woman in space?', 'True or false: 5 kilometer == 5000 meters?', '(5 + 3)/2 * 10 = ?', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", 'What is the minimum crew size for the ISS?'];
 
+let correctAnswers;
 correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
 
+let candidateAnswers;
 candidateAnswers = []
 
 function askForName() {
@@ -50,6 +57,7 @@ function gradeQuiz(candidateAnswers) {
 
 function runProgram() {
   askForName();
+  console.log(`\nHello, ${candidateName} \n`)
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
